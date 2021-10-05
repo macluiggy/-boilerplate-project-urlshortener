52fcdd02e2ddb2b5a3963c4dc6e8fa6b546cd8f0
Luiggy Macias <ferrinluiggy@gmail.com>
2021-10-05 17:28:20 +0000

    using __dirname

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-//pilas
+//pilasd
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
+//app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(`${__dirname}/public`));
+//console.log(process.cwd());
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  //res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
+  //res.json({ greeting: process.cwd(), a: __dirname });
   res.json({ greeting: 'hello API' });
 });
 
