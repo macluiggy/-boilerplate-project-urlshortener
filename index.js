48aaaf61c048a65713d99e57ffe840ca5b72e8c0
macluiggy <ferrinluiggy@gmail.com>
2021-10-06 10:48:10 -0500

    modified files and adding mongo and mongoose

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config({ path: './sample.env' });
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
 //pilasd
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,6 +24,34 @@ app.get('/api/hello', function(req, res) {
   //res.json({ greeting: process.cwd(), a: __dirname });
   res.json({ greeting: 'hello API' });
 });
+
+const uri = process.env.PW
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let urlSchema = new mongoose.Schema({
+  original: { type: String, required: true},
+  short: Number
+})
+
+let Url = mongoose.model('Url', urlSchema)
+//console.log(uri)
+app
+  .use(bodyParser.urlencoded({ extended: false }))
+  .post('/api/shorturl', (req, res) => {
+    const { url } = req.body
+    console.log(url)
+    let test = /https?:\/\/(www)?\.?.+\..+/gi.test(url)
+    console.log(test)
+    if (test) {
+      res.json({
+        original_url: url,
+      })
+    } else {
+      res.json({
+        error: 'Invalid Url'
+      })
+    }
+  })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
